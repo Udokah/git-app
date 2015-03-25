e15e10baaee0066552287412adf400a2b300ef45
Udo <udo@Monsieur-Slim.local>
2015-03-25 14:56:31 +0100

    created issues directive and service

--- app/js/services.js
+++ app/js/services.js
@@ -1,4 +1,25 @@
 
-app.factory('gitService', ['$http', function($http){
+app.factory('gitService', ['$http','settings', function($http,settings){
+    var factory = {};
+    var promise ;
 
+    var parseUrl = function(url){
+        var sides = url.split('/');
+        return{
+            owner: sides[3],
+            repo: sides[4]
+        }
+    };
+
+    factory.getIssues = function(url){
+        var bits = parseUrl(url);
+        promise = $http({
+            method: 'GET',
+            url: settings.baseUrl + '/repos/' + bits.owner + '/' + bits.repo + '/issues',
+            cache: false
+        });
+        return promise;
+    };
+
+    return factory;
 }]);
